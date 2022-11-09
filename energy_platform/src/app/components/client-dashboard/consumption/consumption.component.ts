6056d3533909e0dd082348829d233d005b01d8d4
denisafilip <denisafilip0207@gmail.com>
2022-11-09 08:13:30 +0200

    feature: Finish assignment1

--- energy_platform/src/app/components/client-dashboard/consumption/consumption.component.ts
+++ energy_platform/src/app/components/client-dashboard/consumption/consumption.component.ts
@@ -62,7 +62,7 @@ export class ConsumptionComponent implements OnInit {
         data: [{
           type: "line",
           xValueFormatString: "YYYY",
-          yValueFormatString: "$#,###.##",
+          yValueFormatString: "#,###.##kW",
           dataPoints: this.dataPoints,
         }]
       }
